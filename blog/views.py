66c5e3610abd871e308c532632f89311d4c21464
svillors <tntzator9@gmail.com>
2025-03-12 15:32:16 +0600

    optimize comments count evaluation.

--- blog/views.py
+++ blog/views.py
@@ -21,6 +21,20 @@ def serialize_post(post):
     }
 
 
+def serialize_post_optimized(post):
+    return {
+        'title': post.title,
+        'teaser_text': post.text[:200],
+        'author': post.author.username,
+        'comments_amount': post.comments_amount,
+        'image_url': post.image.url if post.image else None,
+        'published_at': post.published_at,
+        'slug': post.slug,
+        'tags': [serialize_tag(tag) for tag in post.tags.all()],
+        'first_tag_title': post.tags.all()[0].title,
+    }
+
+
 def serialize_tag(tag):
     return {
         'title': tag.title,
@@ -30,20 +44,29 @@ def serialize_tag(tag):
 
 def index(request):
 
-    posts = (
-        Post.objects
-        .annotate(likes_amount=Count('likes'))
-        .order_by('-likes_amount')
+    most_popular_posts = (
+        Post.objects.
+        annotate(likes_count=Count('likes', distinct=True))
+        .order_by('-likes_count')
         .prefetch_related('author')
+    )[:5]
+    most_popular_posts_ids = [post.id for post in most_popular_posts]
+    posts_with_comments = (
+        Post.objects
+        .filter(id__in=most_popular_posts_ids)
+        .annotate(comments_amount=Count('comments'))
     )
-    most_popular_posts = posts[:5]
+    ids_and_comments = posts_with_comments.values_list('id', 'comments_amount')
+    count_for_id = dict(ids_and_comments)
+    for post in most_popular_posts:
+        post.comments_amount = count_for_id[post.id]
 
-    fresh_posts = (
+    most_fresh_posts = (
         Post.objects
-        .order_by('published_at')
+        .annotate(comments_amount=Count('comments'))
+        .order_by('-published_at')
         .prefetch_related('author')
-    )
-    most_fresh_posts = list(fresh_posts)[-5:]
+    )[:5]
 
     tags = Tag.objects.annotate(
         tagged_posts_amount=Count('posts')).order_by('-tagged_posts_amount')
@@ -51,9 +74,9 @@ def index(request):
 
     context = {
         'most_popular_posts': [
-            serialize_post(post) for post in most_popular_posts
+            serialize_post_optimized(post) for post in most_popular_posts
         ],
-        'page_posts': [serialize_post(post) for post in most_fresh_posts],
+        'page_posts': [serialize_post_optimized(post) for post in most_fresh_posts],
         'popular_tags': [serialize_tag(tag) for tag in most_popular_tags],
     }
     return render(request, 'index.html', context)
